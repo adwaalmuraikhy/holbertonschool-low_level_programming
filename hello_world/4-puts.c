dfba0a59fd37723412eb63b53e20e41a6b9325e3
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-10-14 13:45:13 +0000

    Add 4-puts.c using puts (Betty-compliant)

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c -o puts_test
-./puts_test
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
