c1f5920546fdbc2c935ff0057f5896c5ed0438ff
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-10-15 10:29:26 +0000

    Task 5: print numbers 0-9 using printf

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+nano 5-print_numbers.c#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
