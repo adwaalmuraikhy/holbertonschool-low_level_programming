2e59a2336ae0c6b8061fc3094c64b7b76c8d57cc
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-10-24 17:06:09 +0000

    Fix Betty style and implement FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - prints FizzBuzz from 1 to 100
+ * main - Entry point
+ * Description: Prints numbers 1..100 with FizzBuzz rules.
+ * For multiples of 3 print "Fizz", for multiples of 5 print "Buzz",
+ * and for multiples of both print "FizzBuzz". Values are space-separated.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", i);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
 
-        if (i < 100)
-            printf(" ");
-    }
+		if (i < 100)
+			printf(" ");
+	}
 
-    printf("\n");
-    return (0);
+	printf("\n");
+	return (0);
 }
