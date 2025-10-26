3719b890a261e54f428b166de332686510a0f434
adwaalmuraikhu <iadwaalmuraikhy@gmail.com>
2025-10-26 07:18:29 +0000

    Add task 0: reset_to_98 with main.h (Betty-compliant)

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value an int pointer points to, to 98.
+ * @n: Pointer to an integer variable that will be updated.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
