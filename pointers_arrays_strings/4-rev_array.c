508f191a8836a6ed79c99efbb09c114638a86854
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-10-26 20:45:33 +0000

    Task 4: implement reverse_array; fix Betty style

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i, temp;
+	int i, tmp;
 
-    for (i = 0; i < n / 2; i++)
-    {
-        temp = a[i];
-        a[i] = a[n - 1 - i];
-        a[n - 1 - i] = temp;
-    }
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
 }
