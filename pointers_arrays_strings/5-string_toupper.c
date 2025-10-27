b84567d2cf99cdbc94be7479719d2b0d7f3a4892
adwaalmuraikhu <iadwaalmuraikhy@gmail.com>
2025-10-27 08:32:59 +0000

    Implement 5-string_toupper (uppercase conversion) in pointers_arrays_strings

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: pointer to the string to modify
+ *
+ * Return: pointer to the modified string (same address as @s)
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	if (s == 0)
+		return (0);
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - ('a' - 'A');
+		i++;
+	}
+	return (s);
+}
