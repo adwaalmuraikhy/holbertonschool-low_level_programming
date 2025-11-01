25b4cbbc16f1350881451b7a3aad1375648c389e
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 15:30:50 +0000

    Fix Betty style for 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/**\n * main - prints the program name, followed by a new line\n * @argc: number of arguments\n * @argv: array of arguments\n *\n * Return: 0 on success\n */
+/**
+ * main - prints the program name, followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
     (void)argc;
