b37f5e1e1f19cd239f4670774580503d73ae8019
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 22:20:13 +0000

    fix: correct strcpy and header for checker

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,27 +1,22 @@
-/*
- * File: 9-strcpy.c
- * Auth: Adwa Almuraikhy
- */
-
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src (including '\0')
- *           to the buffer pointed to by dest
+ * _strcpy - copies the string pointed to by src to dest
  * @dest: destination buffer
- * @src:  source string
+ * @src: source string
  *
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+    int i = 0;
 
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+
+    return (dest);
 }
