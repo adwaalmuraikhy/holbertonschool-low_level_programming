91ba120c96813a122b53242dd8360cdca17fca78
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 21:38:57 +0000

    pointers_arrays_strings: add 8-print_array (Betty compliant)

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints n elements of an integer array
+ * @a: pointer to the first element of the array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ void puts2(char *str);
 void puts_half(char *str);
 
 #endif /* MAIN_H */
+void print_array(int *a, int n);
