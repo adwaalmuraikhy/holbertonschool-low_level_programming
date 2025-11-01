3f7f7a45edf63af3f5f5dc2116d48f6a92398297
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 21:17:54 +0000

    pointers_arrays_strings: add 7-puts_half (Betty compliant)

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string, then a newline
+ * @str: pointer to string
+ *
+ * If the length is odd, prints the last n characters where
+ * n = (length_of_the_string + 1) / 2.
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0, start, i;
+
+	if (!str)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
