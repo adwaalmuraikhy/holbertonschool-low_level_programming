bbf564bd32e823df8a929a164fb3c07f8b1abece
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 20:58:48 +0000

    pointers_arrays_strings: add 4-print_rev (Betty compliant)

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	if (!s)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	_putchar('\n');
+}
