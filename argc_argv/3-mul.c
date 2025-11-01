d7f2263b11481ea95fcde77be178103658b0bae9
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 17:02:28 +0000

    Add 3-mul.c (multiplication program - Betty compliant)

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of strings containing the arguments
+ *
+ * Return: 0 if successful, 1 if the number of arguments is incorrect
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
