73ff51fd131252af5532cbe62dcfc80c5a0b639f
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 18:10:11 +0000

    Add 7-print_chessboard.c (Betty compliant) and prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,3 +11,4 @@ char *_strstr(char *haystack, char *needle);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
