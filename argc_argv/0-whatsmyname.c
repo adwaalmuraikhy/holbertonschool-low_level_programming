a9b52353000a267ed9e26163c037794e95298eac
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-01 15:28:57 +0000

    Fix 0-whatsmyname.c (correct printf string) + add README

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
-/**
- * main - prints the program name, followed by a new line
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success
- */
+/**\n * main - prints the program name, followed by a new line\n * @argc: number of arguments\n * @argv: array of arguments\n *\n * Return: 0 on success\n */
 int main(int argc, char *argv[])
 {
     (void)argc;
-    printf("
-", argv[0]);
+    printf("%s\n", argv[0]);
     return (0);
 }
