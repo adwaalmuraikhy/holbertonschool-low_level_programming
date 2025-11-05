f8b67b5c123ab208d3543db7b172ff0a974dd2bf
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-05 19:58:21 +0000

    Task 0: minimal dog.h for checker

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,4 @@ struct dog
     char *owner;
 };
 
-/* typedef */
-typedef struct dog dog_t;
-
-/* prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 #endif /* DOG_H */
