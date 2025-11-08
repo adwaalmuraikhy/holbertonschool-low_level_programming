f54313b5d2095bb698155c309537dacdc9ac6e1a
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-08 20:06:43 +0000

    Style: make 0-create_array.c and main.h fully Betty-compliant

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,29 +1,34 @@
+/**
+ * File: 0-create_array.c
+ * Desc: Implementation of create_array.
+ */
+
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * create_array - creates an array of chars and initializes it with c
- * @size: size of the array
- * @c: char used to initialize the array
+ * create_array - Creates an array of chars and initializes it with a char.
+ * @size: The size of the array to allocate.
+ * @c: The char to fill the array with.
  *
- * Return: pointer to the array,
- *         or NULL if size is 0 or if malloc fails
+ * Return: A pointer to the newly allocated array.
+ *         NULL if size is 0 or if malloc fails.
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
+        char *arr;
+        unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+        if (size == 0)
+                return (NULL);
 
-    arr = malloc(size * sizeof(char));
-    if (arr == NULL)
-        return (NULL);
+        arr = malloc(size * sizeof(char));
+        if (arr == NULL)
+                return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = c;
+        for (i = 0; i < size; i++)
+                arr[i] = c;
 
-    return (arr);
+        return (arr);
 }
 
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Desc: Prototypes for the malloc_free project.
+ */
+
 char *create_array(unsigned int size, char c);
 
 #endif /* MAIN_H */
+
