5dff3d32651fa852138597722865c99ec51a0f3a
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-08 16:42:04 +0000

    structures_typedef: add 1-init_dog.c and prototype in dog.h

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: If @d is not NULL, assigns the given values
+ * to the struct dog fields.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == (void *)0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,21 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * File: dog.h
- * Auth: Adwa Almuraikhy
- * Desc: Header that defines a new type struct dog.
- */
-
-#include <stddef.h>
-
 /**
  * struct dog - basic dog structure
  * @name: dog's name (string)
- * @age: dog's age (float)
+ * @age:  dog's age (float)
  * @owner: owner's name (string)
- *
- * Description: Defines a dog with a name, age and owner.
  */
 struct dog
 {
@@ -23,5 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/* prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
