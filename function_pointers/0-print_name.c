e4ff1d6d3aa80b5191ab681d227c6de0db3f0af2
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-11 07:42:38 +0000

    Add README; polish 0-print_name.c (Betty-ready)

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Auth: adwaalmuraikhy
+ * Desc: Implements print_name - calls a function pointer to print a name.
+ */
+
 #include "function_pointers.h"
 
 /**
- * print_name - calls a function to print a name
- * @name: pointer to the name string
- * @f: pointer to a function that takes (char *) and returns void
+ * print_name - Calls a function to print a name.
+ * @name: Pointer to the name string.
+ * @f:    Function pointer that takes (char *) and returns void.
  *
  * Return: Nothing.
  *
- * Description: If either @name or @f is NULL, the function does nothing.
+ * Note: If either @name or @f is NULL, the function does nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
---
+++ function_pointers/README.md
@@ -0,0 +1,5 @@
+0x0E - Function pointers
+Task 0: print_name - calls a function pointer to print a name.
+Files:
+- 0-print_name.c
+- function_pointers.h
