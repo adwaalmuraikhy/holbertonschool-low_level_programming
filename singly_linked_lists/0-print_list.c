028241af413c510edcb721610cf58a4f740c21f9
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-29 15:02:24 +0000

    Fix Betty style for singly linked lists tasks

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,25 +2,28 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements of a list_t list
- * @h: pointer to head of the list
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
 
-        count++;
-        h = h->next;
-    }
-
-    return (count);
+	return (count);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,16 +7,18 @@
  * struct list_s - singly linked list
  * @str: string (malloc'ed)
  * @len: length of string
- * @next: pointer to next node
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 
-#endif
+#endif /* LISTS_H */
