52813a486258c6ab2893229932f5976273a503db
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-29 14:58:51 +0000

    Add singly linked lists tasks 0 and 1

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,17 +7,16 @@
  * struct list_s - singly linked list
  * @str: string (malloc'ed)
  * @len: length of string
- * @next: points to the next node
- *
- * Description: node structure for singly linked list
+ * @next: pointer to next node
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
-#endif /* LISTS_H */
+#endif
