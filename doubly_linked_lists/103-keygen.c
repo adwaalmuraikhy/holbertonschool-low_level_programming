10e4c3cfc2c9b9f7334317bc0cc5b4b91b7a6fd4
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-12-05 17:13:28 +0000

    Clean 103-keygen.c and fix Betty style

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,114 +3,61 @@
 #include <string.h>
 
 /**
- * f1 - compute index of first char
- * @s: username
- *
- * Return: index in charset
- */
-int f1(char *s)
-{
-	int len;
-
-	len = strlen(s);
-	return ((len ^ 0x3b) & 0x3f);
-}
-
-/**
- * f2 - compute index of second char
- * @s: username
- *
- * Return: index in charset
- */
-int f2(char *s)
-{
-	int i;
-	int sum = 0;
-
-	for (i = 0; s[i]; i++)
-		sum += s[i];
-	return ((sum ^ 0x4f) & 0x3f);
-}
-
-/**
- * f3 - compute index of third char
- * @s: username
- *
- * Return: index in charset
- */
-int f3(char *s)
-{
-	int i;
-	int prod = 1;
-
-	for (i = 0; s[i]; i++)
-		prod *= s[i];
-	return ((prod ^ 0x55) & 0x3f);
-}
-
-/**
- * f4_seed - compute seed for rand
- * @s: username
- *
- * Return: seed value
- */
-int f4_seed(char *s)
-{
-	int i;
-	int max;
-
-	max = s[0];
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] > max)
-			max = s[i];
-	}
-	return (max ^ 0xe);
-}
-
-/**
- * main - keygen for crackme5
+ * main - generate key for crackme5
  * @argc: number of arguments
- * @argv: argument vector
+ * @argv: arguments vector
  *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char **argv)
 {
-	const char *g =
-		"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxq"
-		"ZeF3Qa1rPhdKIouk";
-	char key[7];
-	char *user;
-	int i;
-	int sum;
-	int len;
-	int rnd;
+        char *username;
+        char *charset;
+        char key[7];
+        int len;
+        unsigned int hash;
+        int i;
 
-	if (argc != 2)
-		return (1);
+        if (argc != 2)
+                return (1);
 
-	user = argv[1];
+        username = argv[1];
+        charset =
+"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+        len = strlen(username);
 
-	key[0] = g[f1(user)];
-	key[1] = g[f2(user)];
-	key[2] = g[f3(user)];
+        hash = (len ^ 59) & 63;
+        key[0] = charset[hash];
 
-	srand(f4_seed(user));
-	key[3] = g[rand() & 0x3f];
+        hash = 0;
+        for (i = 0; i < len; i++)
+                hash += username[i];
+        key[1] = charset[(hash ^ 79) & 63];
 
-	sum = 0;
-	for (i = 0; user[i]; i++)
-		sum += user[i] * user[i];
-	key[4] = g[(sum ^ 0xef) & 0x3f];
+        hash = 1;
+        for (i = 0; i < len; i++)
+                hash *= username[i];
+        key[2] = charset[(hash ^ 85) & 63];
 
-	len = user[0];
-	rnd = 0;
-	for (i = 0; i < len; i++)
-		rnd = rand();
-	key[5] = g[(rnd ^ 0xe5) & 0x3f];
+        hash = 0;
+        for (i = 0; i < len; i++)
+                if (username[i] > (int)hash)
+                        hash = username[i];
+        srand(hash ^ 14);
+        key[3] = charset[rand() & 63];
 
-	key[6] = '\0';
-	printf("%s", key);
-	return (0);
+        hash = 0;
+        for (i = 0; i < len; i++)
+                hash += username[i] * username[i];
+        key[4] = charset[(hash ^ 239) & 63];
+
+        hash = 0;
+        for (i = 0; i < username[0]; i++)
+                hash = rand();
+        key[5] = charset[(hash ^ 229) & 63];
+
+        key[6] = '\0';
+        printf("%s\n", key);
+
+        return (0);
 }
