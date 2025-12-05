cba881ce5ecc1f118cf002c6a47b6877b8c59888
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-12-05 13:05:55 +0000

    Add add_dnodeint function

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,17 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct dlistint_s - doubly linked list node
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- */
 typedef struct dlistint_s
 {
 	int n;
@@ -21,5 +7,6 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
