b23689b8ecbe75491e5e59a1e5a4d4b32bc75c5f
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-12-05 16:56:03 +0000

    Fix 103-keygen.c to comply with Betty style

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,10 +2,112 @@
 #include <stdlib.h>
 #include <string.h>
 
+char g[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
 /**
- * main - generate a valid key for crackme5
+ * f1 - compute index of first char
+ * @s: username
+ *
+ * Return: index in charset
+ */
+int f1(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return ((len ^ 0x3b) & 0x3f);
+}
+
+/**
+ * f2 - compute index of second char
+ * @s: username
+ *
+ * Return: index in charset
+ */
+int f2(char *s)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; s[i]; i++)
+		sum += s[i];
+	return ((sum ^ 0x4f) & 0x3f);
+}
+
+/**
+ * f3 - compute index of third char
+ * @s: username
+ *
+ * Return: index in charset
+ */
+int f3(char *s)
+{
+	int i;
+	int prod = 1;
+
+	for (i = 0; s[i]; i++)
+		prod *= s[i];
+	return ((prod ^ 0x55) & 0x3f);
+}
+
+/**
+ * f4_seed - compute seed for rand
+ * @s: username
+ *
+ * Return: seed value
+ */
+int f4_seed(char *s)
+{
+	int i;
+	int max;
+
+	max = s[0];
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > max)
+			max = s[i];
+	}
+	return (max ^ 0xe);
+}
+
+/**
+ * f5 - compute index of fifth char
+ * @s: username
+ *
+ * Return: index in charset
+ */
+int f5(char *s)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; s[i]; i++)
+		sum += s[i] * s[i];
+	return ((sum ^ 0xef) & 0x3f);
+}
+
+/**
+ * f6 - compute index of sixth char
+ * @s: username
+ *
+ * Return: index in charset
+ */
+int f6(char *s)
+{
+	int i;
+	int rnd = 0;
+	int len;
+
+	len = s[0];
+	for (i = 0; i < len; i++)
+		rnd = rand();
+	return ((rnd ^ 0xe5) & 0x3f);
+}
+
+/**
+ * main - keygen for crackme5
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: argument vector
  *
  * Return: 0 on success, 1 on error
  */
@@ -13,57 +115,23 @@ int main(int argc, char **argv)
 {
 	char *user;
 	char key[7];
-	char max_char;
-	char *charset;
-	size_t i, j;
-	int rnd;
 
 	if (argc != 2)
 		return (1);
 
 	user = argv[1];
-	charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxq"
-		  "ZeF3Qa1rPhdKIouk";
 
-	/* 1st char */
-	j = (strlen(user) ^ 0x3b) & 0x3f;
-	key[0] = charset[j];
+	key[0] = g[f1(user)];
+	key[1] = g[f2(user)];
+	key[2] = g[f3(user)];
 
-	/* 2nd char */
-	j = 0;
-	for (i = 0; user[i]; i++)
-		j += user[i];
-	key[1] = charset[(j ^ 0x4f) & 0x3f];
+	srand(f4_seed(user));
+	key[3] = g[rand() & 0x3f];
 
-	/* 3rd char */
-	j = 1;
-	for (i = 0; user[i]; i++)
-		j *= user[i];
-	key[2] = charset[(j ^ 0x55) & 0x3f];
-
-	/* 4th char */
-	max_char = user[0];
-	for (i = 0; user[i]; i++)
-		if (user[i] > max_char)
-			max_char = user[i];
-	srand(max_char ^ 0xe);
-	j = rand();
-	key[3] = charset[j & 0x3f];
-
-	/* 5th char */
-	j = 0;
-	for (i = 0; user[i]; i++)
-		j += user[i] * user[i];
-	key[4] = charset[(j ^ 0xef) & 0x3f];
-
-	/* 6th char */
-	max_char = user[0];
-	while (max_char--)
-		rnd = rand();
-	key[5] = charset[(rnd ^ 0xe5) & 0x3f];
-
+	key[4] = g[f5(user)];
+	key[5] = g[f6(user)];
 	key[6] = '\0';
-	printf("%s", key);
 
+	printf("%s", key);
 	return (0);
 }
