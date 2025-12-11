64aa100198311abae31f84d08b8a58dea08f9638
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-12-11 19:00:06 +0000

    Add hash_table_get function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -7,7 +7,8 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: The key (string)
+ *
+ * @key: The key, string
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the list
  */
@@ -20,8 +21,10 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list
  */
 typedef struct hash_table_s
 {
@@ -33,5 +36,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif /* HASH_TABLES_H */
