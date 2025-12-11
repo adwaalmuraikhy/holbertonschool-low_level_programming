102d0bc0b7be1e4a488c13fd53fa1178d23a0ea4
Adwa Almuraikhy <iadwaalmuraikhy@gmail.com>
2025-12-11 08:25:59 +0000

    Add hash table create function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,34 +1,34 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: The key (string)
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the list
+ * @key: The key
+ * @value: The value
+ * @next: A pointer to the next node
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
  * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
+
 #endif /* HASH_TABLES_H */
